e2336bfef82b74888a2e2915bfcfc05a17160807
qtqie <nurfqhah@gmail.com>
2025-01-13 22:45:54 +0800

    kie

---
+++ app/src/main/java/com/example/group03/infolocal.java
@@ -0,0 +1,14 @@
+package com.example.group03;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class infolocal extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infolocal);
+    }
+}
+
