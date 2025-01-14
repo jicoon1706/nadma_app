c4ebae4184384b4bd88e6b75199c6d2b9e17bae5
qtqie <nurfqhah@gmail.com>
2025-01-14 23:19:55 +0800

    kie

--- app/src/main/java/com/example/group03/MainActivity.java
+++ app/src/main/java/com/example/group03/MainActivity.java
@@ -87,5 +87,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void update_info(View view) {
+        Intent intent = new Intent(this, UpdateInfo.class); // Correct target activity
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/group03/infonews.java
+++ app/src/main/java/com/example/group03/infonews.java
@@ -1,12 +1,27 @@
 package com.example.group03;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+
 public class infonews extends AppCompatActivity {
+    Context context;
+    List<ReportDetails> list;
+    EditText etDate, etTitle, etDetails;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +29,11 @@ public class infonews extends AppCompatActivity {
     }
     public void info_page(View view) {
         Intent intent = new Intent(this, addInfo.class); // Correct target activity
+        startActivity(intent);
+    }
+
+    public void report_page(View view) {
+        Intent intent = new Intent(this, Report.class); // Correct target activity
         startActivity(intent);
     }
 
@@ -25,6 +45,17 @@ public class infonews extends AppCompatActivity {
         Intent intent = new Intent(this, infonews.class); // Correct target activity
         startActivity(intent);
     }
+
+    public void info_local(View view) {
+        Intent intent = new Intent(this, infolocal.class); // Correct target activity
+        startActivity(intent);
+    }
+
+    public void update_info(View view) {
+        Intent intent = new Intent(this, UpdateInfo.class); // Correct target activity
+        startActivity(intent);
+    }
+
 }
 
 
